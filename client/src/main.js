42ae6f6aec1905750c224e25a761c57d1fcb05cc
ahmed-dinar <madinar.cse@gmail.com>
2017-07-03 05:38:25 +0600

    add vuejs as client

---
+++ client/src/main.js
@@ -0,0 +1,40 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
+import BootstrapVue from 'bootstrap-vue';
+import VeeValidate from 'vee-validate';
+import axios from 'axios';
+
+import App from './App';
+import router from './router';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'font-awesome/css/font-awesome.css';
+import 'nprogress/nprogress.css';
+import './assets/style.css';
+import './assets/fonts/fonts.css';
+
+sync(store, router);
+
+Vue.prototype.$http = axios;
+Vue.prototype.$http.defaults.headers.common.Authorization = store.getters.getToken;
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate, {
+  errorBagName: 'formError',
+  fieldsBagName: 'formFields'
+});
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+});
